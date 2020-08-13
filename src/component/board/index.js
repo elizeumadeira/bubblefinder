600f708f27839f68bd32c8b36885e7b8e6afa4a6
Elizeu Madeira <elizeu@infotv.com.br>
2020-08-13 14:03:46 -0300

    Cell click effect added

--- src/component/board/index.js
+++ src/component/board/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import Cell from '../cell';
 
@@ -9,10 +9,11 @@ function Coluna(linhas) {
 }
 
 function Board({ board_config, color_number, color_list }) {
-    var line_key = 0;
-    var column_key = 0;
-    var markey_cell1 = null;
-    var markey_cell2 = null;
+    let line_key = 0;
+    let column_key = 0;
+    const [markey_cell1, setCell1] = useState(null);
+    const [markey_cell2, setCell2] = useState(null);
+    const [board_matrix, setBoardMatrix] = useState({});
 
     // [1, 1],
     // [1, 1],
@@ -38,37 +39,75 @@ function Board({ board_config, color_number, color_list }) {
         gridTemplateRows: `repeat(${board_config.length}, 40px [col-start])`
     };
 
-    const board_matrix = board_config.map(
+    const board_matrix_component = board_config.map(
         (linhas) => {
             line_key++;
             column_key = 0;
             return linhas.map((celula) => {
                 column_key++;
-                return <Cell
-                    disabled={celula == 0}
-                    key={`${line_key}_${column_key}`}
-                    color={random_colors.splice(0, celula)}
-                    onClick={() => {
-                        if (markey_cell1 == null) {
-                            markey_cell1 = `${line_key}_${column_key}`;
-                        }
-                        else {
-                            markey_cell2 = `${line_key}_${column_key}`;
-                            console.log(markey_cell1, markey_cell1);
-                        }
-                    }}
-                />
-            }
-            )
-        }
-    );
+                let key = `${line_key}_${column_key}`;
+                var b = {};
+                b[key] = null;
+                b[key] = {
+                    disabled: celula == 0,
+                    key: key,
+                    color: celula == 0 ? null : random_colors.splice(0, celula)[0],
+                    clicked: false
+                };
 
+                return b;
+            }
+            ).flat()
+        }
+    ).flat().reduce((accumulator, currentValue) => Object.assign(accumulator, currentValue));
+
+    useEffect(() => {
+        if (JSON.stringify(board_matrix) == '{}') {
+            console.log('renderiza board');
+            setBoardMatrix(board_matrix_component);
+        }
+
+        if (markey_cell1 != null & markey_cell2 != null) {
+
+            if (markey_cell1 == markey_cell2)
+                return;
+
+            console.log('compara', markey_cell1, markey_cell2);
+
+            setCell1(null);
+            setCell2(null);
+            return;
+        }
+    }, [markey_cell2, markey_cell1, board_matrix]);
+
+    function actionClick(key) {
+        if (markey_cell1 == null) {
+            setCell1(key);
+        } else {
+            if (markey_cell1 != key) {
+                setCell2(key);
+            }
+        }
+
+        var b = {};
+        b[key] = {clicked: true}
+
+        setBoardMatrix({...board_matrix, ...b});
+    }
     return (
         <>
             <div>board</div>
             <div className='container' style={stylecontainer}>
                 {
-                    board_matrix
+                    Object.entries(board_matrix).map(([key, object]) =>
+                        <Cell
+                            disabled={object.disabled}
+                            key={key}
+                            color={object.color}
+                            clicked={object.clicked}
+                            onClick={() => actionClick(key)}
+                        />
+                    )
                 }
             </div>
         </>
