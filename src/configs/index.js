408f36e80f9e89529d598679eb6514e135732432
Elizeu Madeira <elizeu@infotv.com.br>
2020-08-18 12:33:22 -0300

    10 levels added

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,23 +1,133 @@
 import { getLocalStorage } from './store';
 const LOCAL_STORAGE_KEY = '__buublefinder__';
 const default_level = {
+    1: {
+        unlock: true,
+        score: 0,
+        color_number: 2,
+        tries: 2,
+        board: [
+            [1, 1]
+        ]
+    },
+    2: {
+        unlock: true,
+        score: 0,
+        color_number: 4,
+        tries: 3,
+        board: [
+            [1, 1],
+            [1, 1]
+        ]
+    },
     3: {
         unlock: true,
         score: 0,
         color_number: 8,
-        tries: 4,
+        tries: 6,
         board: [
-            // [1,1,1,1],
-            // [1,0,0,1],
-            // [1,0,0,1],
-            // [1,1,1,1],
-
             [1, 1],
             [1, 1],
             [1, 1],
             [1, 1],
         ]
-    }
+    },
+    4:  {
+        unlock: false,
+        score: 0,
+        color_number: 12,
+        tries: 8,
+        board: [
+            [1,1,1,1],
+            [1,0,0,1],
+            [1,0,0,1],
+            [1,1,1,1],
+        ]
+    },
+    5:  {
+        unlock: false,
+        score: 0,
+        color_number: 16,
+        tries: 18,
+        board: [
+            [0,0,1,0,0,0],
+            [0,0,1,1,0,0],
+            [0,1,1,1,1,1],
+            [1,1,1,1,1,0],
+            [0,0,1,1,0,0],
+            [0,0,0,1,0,0]
+        ]
+    },
+    6:  {
+        unlock: false,
+        score: 0,
+        color_number: 16,
+        tries: 18,
+        board: [
+            [1,1,0,0,1,1],
+            [1,1,0,0,1,1],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [1,1,0,0,1,1],
+            [1,1,0,0,1,1]
+        ]
+    },
+    7:  {
+        unlock: false,
+        score: 0,
+        color_number: 18,
+        tries: 18,
+        board: [
+            [1,1,1,0,0,0],
+            [1,1,1,0,0,0],
+            [1,1,1,0,0,0],
+            [0,0,0,1,1,1],
+            [0,0,0,1,1,1],
+            [0,0,0,1,1,1]
+        ]
+    },
+    8:  {
+        unlock: false,
+        score: 0,
+        color_number: 18,
+        tries: 18,
+        board: [
+            [1,0,1,0,1,0],
+            [0,1,0,1,0,1],
+            [1,0,1,0,1,0],
+            [0,1,0,1,0,1],
+            [1,0,1,0,1,0],
+            [0,1,0,1,0,1]
+        ]
+    },
+    9:  {
+        unlock: false,
+        score: 0,
+        color_number: 20,
+        tries: 18,
+        board: [
+            [1,1,0,0,1,1],
+            [1,1,0,0,1,1],
+            [0,0,1,1,0,0],
+            [0,0,1,1,0,0],
+            [1,1,0,0,1,1],
+            [1,1,0,0,1,1]
+        ]
+    },
+    10:  {
+        unlock: false,
+        score: 0,
+        color_number: 20,
+        tries: 18,
+        board: [
+            [1,1,0,0,1,1],
+            [0,1,1,1,1,0],
+            [0,0,1,1,0,0],
+            [0,0,1,1,0,0],
+            [0,1,1,1,1,0],
+            [1,1,0,0,1,1]
+        ]
+    },
 };
 
 const levels = (function () {
